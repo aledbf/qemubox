09dfeac0ae3f6a9e13d602eea7607859994b2c67
Manuel de Brito Fontes <aledbf@gmail.com>
2025-12-04 15:12:18 -0300

    Add missing import for beacon containerd services

--- cmd/vminitd/main.go
+++ cmd/vminitd/main.go
@@ -32,6 +32,7 @@ import (
 	_ "github.com/aledbf/beacon/containerd/api/services/bundle/v1"
 	_ "github.com/aledbf/beacon/containerd/api/services/system/v1"
 
+	_ "github.com/aledbf/beacon/containerd/services"
 	_ "github.com/aledbf/beacon/containerd/vminit/events"
 	_ "github.com/aledbf/beacon/containerd/vminit/streaming"
 )
